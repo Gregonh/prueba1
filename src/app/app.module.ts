b38ceaae7019227bef4628aca4d048e506708739
Grego_nh <gregonh@gmail.com>
2020-05-31 04:35:35 +0200

    añadido formulario reactivo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+// import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CorreoComponent } from './correo/correo.component';
 import { ListaCorreosComponent } from './lista-correos/lista-correos.component';
+import { NuevoCorreoComponent } from './nuevo-correo/nuevo-correo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CorreoComponent,
-    ListaCorreosComponent
+    ListaCorreosComponent,
+    NuevoCorreoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
